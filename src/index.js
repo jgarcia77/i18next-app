6072741d98d68af56608aaeda18b0aa499eb96f1
Josue Garcia <JosueG@magenic.com>
2018-10-09 14:44:02 -0500

    Basic Example

--- src/index.js
+++ src/index.js
@@ -3,6 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import i18next from 'i18next';
+import LngDetector from 'i18next-browser-languagedetector';
+
+i18next
+    .use(LngDetector)
+    .init({
+    fallbackLng: 'en',
+    debug: true,
+    detection: {
+        order: ['navigator']
+    },
+    resources: {
+      en: {
+        translation: {
+          "greet": "Hello"
+        }
+      },
+      es: {
+        translation: {
+            "greet": "Hola"
+          }
+      },
+      de: {
+        translation: {
+            "greet": "Hallo"
+          }
+      }
+    }
+  }, function(err, t) {
+    // initialized and ready to go!
+  });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- src/pages/main/BasicExample.js
+++ src/pages/main/BasicExample.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import i18next from 'i18next';
 
 export default class BasicExample extends Component {
     render() {
         return (
             <div>
-                <h2>Basic Example</h2>
+                <h2>Basic Example: {i18next.language}</h2>
+                {i18next.t('greet')}!
             </div>
         );
     }
